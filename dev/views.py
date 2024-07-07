d2013cfffe522ddccba6889b508e0a484b70bfd8
Devesh Shrivas <114324384+Deveshshrivas@users.noreply.github.com>
2024-07-07 22:08:10 +0530

    first commit

---
+++ dev/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from rest_framework.views import APIView, Response
+
+# Create your views here.
+class MessageAPIView(APIView):
+    def get(self, request):
+        pass
